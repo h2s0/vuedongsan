f179e6b3c6e4ad542d16d54819daaf1db0db5f93
heeso <khs@alpinelab.io>
2024-04-23 13:10:02 +0900

    ✨ vue 2강 : 데이터바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    원룸샵
+    <h4>{{ products[0] }}</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      price1 : 50,
+      price2 : 60,
+      products: ['역삼동원룸', '화곡역원룸', '마포구투룸']
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
